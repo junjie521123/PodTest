8d3596e3dd405826fa2b391795707b1a3ba9a8ef
lijunjie <lijunjie@xqshijie.cn>
2016-06-06 15:51:11 +0800

    提交podtest
    提交podTest

--- PodTest/ViewController.m
+++ PodTest/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <MBProgressHUD.h>
 
 @interface ViewController ()
 
@@ -17,6 +18,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+
 }
 
 - (void)didReceiveMemoryWarning {
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '7.0'
+
+def pods
+
+  pod 'AFNetworking', '~> 3.1.0'
+  pod 'MBProgressHUD', '~> 0.8'
+end 
+
+target 'PodTest' do
+  pods
+end
